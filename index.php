096e8deb00c67100936399b0a0302838582ab55f
Julio Rodriguez <juliovr_93@hotmail.com>
2017-07-07 20:54:27 -0500

    Proyecto Tecnocom

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="Valle Rodriguez Julio Cesar">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>TecnoCom</title>
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+
+  </head>
+
+  <!-- body ================================================== -->
+  <body>
+    
+    <div class="container-fluid">
+    
+      <!-- wrapper ================================================== -->
+      <div class="row" id="id_wrapper">
+        
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+          <!-- navbar ================================================== -->
+          <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+              
+              <!-- navbar-header ================================================== -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">TecnoCom</a>
+              </div><!--/.navbar-header -->
+              
+              <!-- navbar-collapse ================================================== -->
+              <div id="navbar" class="navbar-collapse collapse">
+                <form class="navbar-form navbar-right">
+                  <div class="form-group">
+                    <input type="text" placeholder="Email" class="form-control input-sm">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" placeholder="Password" class="form-control input-sm">
+                  </div>
+                  <div class="btn-group" role="group" aria-label="...">
+                    <button type="submit" class="btn btn-success btn-sm">Sign in</button>
+                    <button type="button" class="btn btn-primary btn-sm">Registrarse</button>
+                  </div>
+                </form>
+              </div><!--/.navbar-collapse -->
+
+            </div>
+          </nav><!--/.navbar -->
+
+          <!-- content ================================================== -->
+          <div class="row" id="id_content">
+            
+            <!-- carousel ================================================== -->
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              
+              <!-- indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+              </ol>
+
+              <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+                  <div class="container">
+                    <div class="carousel-caption">
+                      <h1>Imagen 1</h1>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="item">
+                  <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+                  <div class="container">
+                    <div class="carousel-caption">
+                      <h1>Imagen 2</h1>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="item">
+                  <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+                  <div class="container">
+                    <div class="carousel-caption">
+                      <h1>Imagen 3</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+
+              <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+
+            </div><!-- /.carousel -->
+
+          </div><!-- /.content -->
+        
+        </div>
+
+      </div><!-- /.wrapper -->
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    
+  </body>
+</html>
